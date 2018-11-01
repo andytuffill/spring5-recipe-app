e21a7a2a6fb6e1584eff40997cff6c4987528ed3
Andy Tuffill <andy.tuffill@gmail.com>
2018-11-01 21:25:56 +0000

    After lecture 147 - added JPA query methods for find category (and UoM) by description

--- src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
@@ -1,14 +1,35 @@
 package com.example.spring5recipeapp.controllers;
 
+import com.example.spring5recipeapp.domain.Category;
+import com.example.spring5recipeapp.domain.UnitOfMeasure;
+import com.example.spring5recipeapp.repositories.CategoryRepository;
+import com.example.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-        System.out.println("something to say...");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println("cat id is " + categoryOptional.get().getId());
+        System.out.println("uom id is " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
